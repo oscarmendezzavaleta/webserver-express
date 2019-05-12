714c5d0e447d72031332c4a22e4781b27b1a76ca
Oscar <oscar.mendez.zavaleta@gmail.com>
2019-05-12 12:41:47 -0500

    primer

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales/');
+
+app.set('view engine', 'hbs');
+
+//helpers
+
+
+app.get('/', (req, res) => {
+
+
+    res.render('home', {
+        nombre: 'Oscar'
+    });
+
+
+});
+
+
+app.get('/about', (req, res) => {
+
+
+    res.render('about');
+
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`escuchando peticiones en el puerto ${port}`);
+});
